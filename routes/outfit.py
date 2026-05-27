9f982edcdb1645ff4997e76762b75b2d71ba0d79
Sherlyn Muthoni <sherlynmuthoni52@gmail.com>
2026-05-27 17:50:37 +0300

    Add outfit routes for suggestions and history with login requirement

--- routes/outfit.py
+++ routes/outfit.py
@@ -1,3 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
 
-outfit_bp = Blueprint('outfit', __name__)
+outfit_bp = Blueprint("outfit", __name__)
+
+@outfit_bp.route("/suggestions")
+@login_required
+def suggestions():
+    return render_template("suggestions.html")
+
+@outfit_bp.route("/history")
+@login_required
+def history():
+    return render_template("history.html")
