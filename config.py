c47f97fa093eeb899408ba05f7ab0b48f9dd47e9
Sherlyn Muthoni <sherlynmuthoni52@gmail.com>
2026-05-29 21:04:40 +0300

    Update WEATHER_API_KEY in config.py to the correct value

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 SECRET_KEY = "closetiq-secret-key-2025"
 UPLOAD_FOLDER = "static/uploads"
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
-WEATHER_API_KEY = "paste-your-openweathermap-key-here"
+WEATHER_API_KEY = "57afe33e7588afac53705e3caf3750a4"
 DATABASE = "closetiq.db"
